94e458ff4173b4fc3e92e795068fd607c5f424ba
krisyu <kristinyu@126.com>
2020-03-17 18:37:50 +0800

    1.Get and post request test in django framwork

--- krisPython/urls.py
+++ krisPython/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from kris_python import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^view/',views.fun),
+    url(r'^get',views.getRequest),
+    url(r'^post', views.postRequest)
 ]
--- kris_python/views.py
+++ kris_python/views.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def fun(request):
+    return HttpResponse("ddddd")
+
+
+def getRequest(request):
+    request.encoding='utf-8'
+    if 'q' in request.GET and request.GET['q']:
+        mess='ur input param is'+request.GET['q']
+    else:
+        mess='input error'
+    return HttpResponse(mess)
+
+def postRequest(request):
+    ctx={}
+    if request.POST:
+        ctx['rtx'] = request.POST['a']
+        mess = "ur post request params are"+ctx
+    else:
+        mess = 'ur post error accure'
+    return render(request,ctx)
+
